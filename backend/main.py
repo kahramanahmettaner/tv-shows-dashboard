f1d6604b8e72bc406eaf1aed6188afe321d8bfc2
AhmetTanerKahraman <kahramanahmettaner@gmail.com>
2024-02-26 23:32:54 +0100

    feat: Update main.py to handle dynamic TV show search
    - Updated main.py to include route '/shows' for dynamic TV show search based on show_name parameter
    
    - Implemented error handling for missing show_name parameter and exceptions during show search
    
    - Adjusted response handling to return appropriate HTTP status codes and messages

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from imdb import ImdbShow
 
 app = Flask(__name__)
 
@@ -9,10 +10,24 @@ def home():
 
 
 @app.route('/shows', methods=['GET'])
-def show():
-    from imdb import ImdbShow
-    shows = ImdbShow.search_for_shows('Breaking Bad')
-    return jsonify(shows), 200
+def shows_by_title():
+    show_name = request.args.get('show_name')
+    if show_name is None:
+        return jsonify({"error": "show_name parameter is missing"}), 400
+
+    try:
+        query_results = ImdbShow.search_for_shows(show_name)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+    if query_results is None:
+        return jsonify({"error": "An error occurred while searching for shows"}), 500
+
+    if not query_results:
+        return jsonify({"message": "No shows found for the provided show_name"}), 404
+
+    # If everything is successful, return the query_results
+    return jsonify(query_results), 200
 
 
 app.run()
