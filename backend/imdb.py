5dcb268c0653bff7017a3cb09fbba9036d98430c
AhmetTanerKahraman <kahramanahmettaner@gmail.com>
2024-03-02 23:29:44 +0100

    refactor: Improve error handling and error messages in ImdbShow class
    - Modified parse_show_name() and parse_seasons_count() methods to include error handling for robustness
    
    - Added detailed error messages to exceptions raised in case of parsing errors
    
    - Enhanced fetch_show_data() method to handle different types of exceptions, including HTTP errors and unexpected errors, with informative error messages

--- backend/imdb.py
+++ backend/imdb.py
@@ -56,19 +56,42 @@ class ImdbShow:
         return shows
 
     def parse_show_name(self, soup):
-        h2_element = soup.find_all("h2",  {'data-testid': 'subtitle'})[0]
-        self.show_name = h2_element.text
+        try:
+            h2_element = soup.find_all("h2",  {'data-testid': 'subtitle'})[0]
+            self.show_name = h2_element.text
+        except Exception as e:
+            raise Exception("Error parsing show name:", e)
 
     def parse_seasons_count(self, soup):
-        season_numbers_container = soup.find_all("ul", class_="ipc-tabs ipc-tabs--base ipc-tabs--align-left")[0]
-        season_numbers = season_numbers_container.find_all('a')
-        seasons_count = season_numbers[-1].text
-        self.seasons_count = int(seasons_count)
+        try:
+            season_numbers_container = soup.find_all("ul", class_="ipc-tabs ipc-tabs--base ipc-tabs--align-left")[0]
+            season_numbers = season_numbers_container.find_all('a')
+            seasons_count = season_numbers[-1].text
+            self.seasons_count = int(seasons_count)
+        except Exception as e:
+            raise Exception("Error parsing seasons count:", e)
 
     def fetch_show_data(self):
-        req_url = self.season_url + '1'
-        result = requests.get(req_url, headers=self.headers)
-        soup = BeautifulSoup(result.content, features="html.parser")
+        try:
+            req_url = self.season_url + '1'
+            result = requests.get(req_url, headers=self.headers)
+            result.raise_for_status()  # Raises an exception for 4xx or 5xx status codes
+            soup = BeautifulSoup(result.content, features="html.parser")
 
-        self.parse_seasons_count(soup)
-        self.parse_show_name(soup)
+            self.parse_seasons_count(soup)
+            self.parse_show_name(soup)
+
+        except requests.RequestException as e:
+            # Check if the exception is an HTTPError
+            if isinstance(e, requests.HTTPError):
+                # If it is, raise an exception with the error message including the error type and status code
+                raise Exception(f"Error fetching show data: HTTPError {e.response.status_code}")
+            else:
+                # If it's not an HTTPError, raise an exception with just the error type
+                raise Exception("Error fetching show data: " + type(e).__name__)
+
+            # to send also the error message from imdb
+            # raise Exception("Error fetching show data:", e)
+
+        except Exception as e:
+            raise Exception("An unexpected error occurred:", e)
