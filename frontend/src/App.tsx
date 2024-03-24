3a583ff1e8b4923e7814413048cc7736a5fd8172
AhmetTanerKahraman <kahramanahmettaner@gmail.com>
2024-03-24 22:40:53 +0100

    feat(frontend): Update App component to include routing layout with Navbar and Footer
    - Modified App.tsx to implement routing layout using react-router-dom
    
    - Added Navbar and Footer components with placeholder code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,26 @@
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, Outlet } from "react-router-dom"
+import Navbar from "./components/navbar/Navbar"
+import Footer from "./components/footer/Footer"
 
 function App() {
+
+  const Layout = () => {
+    return (
+      <div className='main'>
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </div>
+    )
+  } 
+
   return (
     <Routes>
-      <Route path="/" element={<h1>App</h1>} />
+      <Route path="/" element={<Layout />}>
+
+        <Route path="" element={<h1>App</h1>} />
+
+      </Route>
     </Routes>
   )
 }
