36c5cf50d2dcd56f3ed8db6f18ef532b4d9b5385
AhmetTanerKahraman <kahramanahmettaner@gmail.com>
2024-03-25 23:13:00 +0100

    feat(frontend): Update Navbar component to include styled elements and icons
    - Updated Navbar.tsx to include styled elements using CSS modules
    
    - Added navbar.module.css for styling Navbar component

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -1,6 +1,24 @@
+import styles from './navbar.module.css'
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className={styles.navbar}>
+
+      <div className={styles.logo}>
+        <span>TV Shows Dashboard</span>
+      </div>
+
+      <div className={styles.icons}>
+        <img src='/search.svg' alt='' className={styles.icon}/>
+        <img src='/app.svg' alt='' className={styles.icon}/>
+        <div className={styles.user} >
+          <img src='/' alt='' className={styles.icon}/>
+          <span>Jane Doe</span>
+        </div>
+        <img src='/settings.svg' alt='' className={styles.icon}/>
+      </div>
+
+    </div>
   )
 }
 
