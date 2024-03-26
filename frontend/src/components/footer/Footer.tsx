66131c5baf0f0134ca80b38698c9a61aae7d5798
AhmetTanerKahraman <kahramanahmettaner@gmail.com>
2024-03-26 22:57:24 +0100

    feat(frontend): Update Footer component with styled elements
    - Updated Footer.tsx to include styled elements using CSS modules
    
    - Added footer.module.css for styling Footer component

--- frontend/src/components/footer/Footer.tsx
+++ frontend/src/components/footer/Footer.tsx
@@ -1,6 +1,11 @@
+import styles from './footer.module.css'
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className={styles.footer}>
+      <span>About Us</span>
+      <span>&copy; TV Shows Dashboard</span>
+    </div>
   )
 }
 
