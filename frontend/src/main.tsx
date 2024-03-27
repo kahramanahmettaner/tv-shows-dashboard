d12b014d56b2e3535b763d15e28e34671966efd5
AhmetTanerKahraman <kahramanahmettaner@gmail.com>
2024-03-27 23:58:50 +0100

    style(frontend): add variables.css, global.css and organize CSS files
    - Added variables.css to define global CSS variables for background colors and text colors
    
    - Added global.css to include global styling for the entire application
    
    - Organized CSS files (variables.css, reset.css, global.css) into a 'styles' folder
    
    - Imported CSS files in main.tsx for global styling

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './reset.css'
-import './index.css'
+import './styles/variables.css'
+import './styles/reset.css'
+import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
