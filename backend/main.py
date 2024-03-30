0be2eabb83be3544c77db374a680e3fed4187ad1
AhmetTanerKahraman <kahramanahmettaner@gmail.com>
2024-03-30 01:59:36 +0100

    feat(backend): enable CORS with Flask-CORS
    - Installed Flask-CORS package to handle Cross-Origin Resource Sharing (CORS) in the Flask app
    
    - Added CORS(app) to enable CORS for all routes in the Flask application

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify, request
 from imdb import ImdbShow
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # This will enable CORS for all routes in the Flask app
 
 
 @app.route('/', methods=['GET'])
