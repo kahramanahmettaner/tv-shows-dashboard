06d800c87e2f832f3520aa96ab7e6fedfcf519f5
AhmetTanerKahraman <kahramanahmettaner@gmail.com>
2024-04-04 04:48:28 +0200

    feat(frontend): update SearchBar coomponent with input field
    - Updated SearchBar component to include an input field for user input
    
    - Added state management for the input field using useState hook
    
    - Implemented handleInput function to update input state on user input
    
    The function handleSubmit is not implemented yet
    
    - Added dependencies for FontAwesome for search icon
    
    - Added FontAwesomeIcon for the search button

--- frontend/src/components/searchBar/SearchBar.tsx
+++ frontend/src/components/searchBar/SearchBar.tsx
@@ -1,5 +1,8 @@
 import styles from './searchBar.module.css'
 import { Dispatch, SetStateAction } from 'react'
+import { useState } from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons"
 
 interface SearchBarProps {
     setLoading: Dispatch<SetStateAction<boolean>>;
@@ -7,9 +10,31 @@ interface SearchBarProps {
 }
 
 const SearchBar = ({ setLoading, setErrorMessage }: SearchBarProps) => {
-    
+
+    const [input, setInput] = useState('')
+
+    const handleInput = (e: any) => {
+        const currentInput = e.target.value
+        setInput(currentInput)
+    }   
+
+    const handleSubmit = (e: any) => {
+        e.preventDefault()
+        setLoading(true)
+
+        // TODO: implement
+        // fetch shows by showname
+        setTimeout(()  => {setErrorMessage('Not Implemented')}, 2000);   
+    }
+
+
     return (
-        <div>SearchBar</div>
+        <form className={styles["input-wrapper"]}>
+            <button type='submit' onClick={ (e) => {handleSubmit(e)} }>
+                <FontAwesomeIcon icon={faMagnifyingGlass} />
+            </button>
+            <input type="search" placeholder="Type to search for a tv show" value={input} onChange={ (e) => {handleInput(e)} }/>
+        </form>
     )
 }
 
