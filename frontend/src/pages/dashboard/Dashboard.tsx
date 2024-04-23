50785fc7e35e4bcf41bbe1a84a1e82715b2ea9c7
AhmetTanerKahraman <kahramanahmettaner@gmail.com>
2024-04-24 00:23:14 +0200

    style(frontend): update Dashboard layout using CSS grid
    - Added CSS classes to div elements in Dashboard component for better layout organization
    
    - Updated dashboard.module.css to define grid layout and styling for the dashboard boxes

--- frontend/src/pages/dashboard/Dashboard.tsx
+++ frontend/src/pages/dashboard/Dashboard.tsx
@@ -4,14 +4,16 @@ import styles from './dashboard.module.css'
 const Dashboard = () => {
   return (
     <div className={styles.dashboard}>
-      <div>
+
+      <div className={`${styles.box} ${styles.box1}`}>
         <TopBox />
       </div>
-      <div>Box 2</div>
-      <div>Box 3</div>
-      <div>
+      <div className={`${styles.box} ${styles.box2}`}>Box 2</div>
+      <div className={`${styles.box} ${styles.box3}`}>Box 3</div>
+      <div className={`${styles.box} ${styles.box4}`}>
         <TopBox />
       </div>
+      
     </div>
   )
 }
