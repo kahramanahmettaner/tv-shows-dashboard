6996c5605c791b69a0a1b8b8ad7763082a7606c0
AhmetTanerKahraman <kahramanahmettaner@gmail.com>
2024-05-18 21:43:43 +0200

    feat(frontend): add new reusable component HorizontalBarChart
    - This new reusable component can be used in other components to display data in horizontal bar chart

---
+++ frontend/src/components/horizontalBarChart/HorizontalBarChart.tsx
@@ -0,0 +1,38 @@
+import styles from './horizontalBarChart.module.css'
+
+type TAttributNames = {
+    title: string;
+    levelText: string;
+    levelValue: string;
+    color: string;
+}
+type Props = {
+    item: any;
+    attributeNames: TAttributNames;
+}
+
+const HorizontalBarChart: React.FC<Props> = ({ item, attributeNames }) => { 
+    
+    return (
+        <div className={styles['container']}>
+            <div className={styles.text}>
+                <p>{item[attributeNames.title]}</p>
+                <p>{item[attributeNames.levelText]}</p>
+            </div>
+            <div className={styles['horizontal-chart']}>
+                <div className={styles['filled']} style={{
+                    height: "100%",
+                    width: `${item[attributeNames.levelValue]}%`,
+                    backgroundColor: `${item[attributeNames.color]}`,
+                    transition: 'width 0.5s'
+                }}>
+
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default HorizontalBarChart
