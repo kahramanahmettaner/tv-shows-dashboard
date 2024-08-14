42b8df01f7b1d1ca434365b11864a13104d60376
AhmetTanerKahraman <kahramanahmettaner@gmail.com>
2024-08-14 22:42:10 +0200

    feat(frontend): enhance TopCredits component with dynamic category selection
    - Updated  component to dynamically display data for cast, writers, and directors based on user selection
    
    - Integrated the reusable  component to allow users to switch between viewing top cast, writers, and directors
    
    - Adjusted state management in  to handle different categories and update the displayed data accordingly

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,27 +3,13 @@ import Navbar from "./components/navbar/Navbar"
 import Footer from "./components/footer/Footer"
 import Dashboard from "./pages/dashboard/Dashboard"
 import Home from "./pages/home/Home"
-import Dropdown from "./components/dropdown/Dropdown"
-import { useState } from "react"
 
 function App() {
-
-  const items = ["item1", "item2", "item3"];
-  const [selectedIndex, setSelectedIndex] = useState(0);
-
   const Layout = () => {
     return (
       <div className='page-content'>
         <Navbar />
         <Outlet />
-
-        {selectedIndex}
-        <Dropdown 
-          dropdownItems={items}
-          selectedIndex={selectedIndex}
-          setSelectedIndex={setSelectedIndex}
-        />
-
         <Footer />
       </div>
     )
--- frontend/src/components/topCredits/TopCredits.tsx
+++ frontend/src/components/topCredits/TopCredits.tsx
@@ -1,27 +1,46 @@
+import { useState } from "react";
 import { useShowDetailsStore } from "../../store/showDetailsStore"
+import { ICredits } from "../../types";
 import TopBox from "../topBox/TopBox";
+import Dropdown from "../dropdown/Dropdown";
 
 const TopCredits = () => {
+    
+    // Credits Data
     const credits = useShowDetailsStore( state => state.credits )
     const loadingCredits = useShowDetailsStore( state => state.loadingCredits )
     const errorMessageCredits = useShowDetailsStore( state => state.errorMessageCredits )
 
-    // Slice the array to get only the top 10 of cast members
-    const top10CastMembers = credits.cast.slice(0, 10);
 
-    // Sort episodes by imdb_rating in descending order
-    top10CastMembers.sort((a, b) => b.episodes_count - a.episodes_count);
+    // Different Credits Categories and Dropdown
+    // List of categories (e.g., ['cast', 'writers', 'directors'])
+    const categories = [
+        { key: 'cast', title: 'Actors with Most Episodes' },
+        { key: 'directors', title: 'Directors with Most Episodes' },
+        { key: 'writers', title: 'Writers with Most Episodes' },
+    ]
+
+    const [selectedIndex, setSelectedIndex] = useState(0);
     
-    // Add episode_title to each episode object using the season_number and episode_number
-    const top10CastMembersDisplay = top10CastMembers.map(castMember => {
-        const episodes_count_text  = `${castMember.episodes_count} episodes`
-        return { ...castMember, episodes_count_text: episodes_count_text };
+    const selectedCategory = categories[selectedIndex];
+
+    const items = credits[selectedCategory.key as keyof ICredits];
+
+    items.sort((a, b) => b.episodes_count - a.episodes_count);
+
+    const top10 = items.slice(0, 10);
+    
+    const top10Modified = top10.map(creditsItem => {
+        const episodes_count_text  = `${creditsItem.episodes_count} episodes`
+        return { ...creditsItem, episodes_count_text: episodes_count_text };
     })
 
+    // For loading and error states
     const loadingContent = <p>Loading...</p>
 
     const errorContent = <p>Error: {errorMessageCredits}</p>
 
+    // Return component jsx
     return (
         <>
             {/* Display loading message while loading */}
@@ -33,15 +52,22 @@ const TopCredits = () => {
             {/* Display fetched shows when there is no error and not loading */}
             {/* Display nothing if there is no show to display */}
             {!loadingCredits && !errorMessageCredits && (
-                top10CastMembersDisplay.length == 0 || 
+                top10Modified.length == 0 || 
+            <>
+            <Dropdown 
+                dropdownItems={categories.map(category => category.title)}
+                selectedIndex={selectedIndex}
+                setSelectedIndex={setSelectedIndex}
+            />
             <TopBox
-                listTitle="Actors with Most Episodes"
-                items={top10CastMembersDisplay}
+                listTitle=""
+                items={top10Modified}
                 attributeNames={{ image: 'image_link', title: 'character', details: 'name', value: 'episodes_count_text' }}
                 titleColor="#FFF"
                 valueColor="#BBA"
                 detailsColor="#978"
             />
+            </>
             )}
         </>
     )
